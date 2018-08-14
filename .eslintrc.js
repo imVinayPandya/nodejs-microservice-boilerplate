c0a615a37d6664695bec60792578748898733513
Vinay Pandya <005vinay@gmail.com>
2018-08-14 12:31:52 +0530

    eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true, // make to not take in any user specified rules in parent folders
+  parser: 'babel-eslint',
+  extends: ['airbnb-base', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        'trailingComma': 'es5',
+        'singleQuote': true,
+        'printWidth': 120,
+        'tabWidth': 2,
+        'useTabs': false
+      }
+    ],
+  },
+  plugins: [
+    'prettier'
+  ]
+};
---
+++ CONTRIBUTING.md
@@ -0,0 +1,12 @@
+# CONTRIBUTING
+
+## filenames:
+
+Regarding filenames most common are short, lower-case filenames. If your file can only be described with two words most JavaScript projects use an underscore as delimiter.
+
+## variables:
+
+Regarding variables same "rules" apply as for filenames. Prototypes or classes however should use camel-case.
+
+## run lint to check linting
+    ```npm run lint .```
